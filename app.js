201d1f695d8fdbcd288db29451e3b0cc3dc8f7d8
markitosha <nataly_106@mail.ru>
2016-08-22 14:05:58 +0300

    Little code review (comments was added)

--- app.js
+++ app.js
@@ -3,45 +3,63 @@
  */
 angular.module('app', [])
     .controller('main', function($scope){
+
+        //Глобальные переменные: Ошибки и дата
+        $scope.emptyName = false;
+        $scope.emptySum = false;
+        $scope.wrongSum = false;
+        $scope.currMonth = 12;
+        $scope.balance = 0;
+
+        //Название месяца по номеру
+        $scope.monthName = function (item) {
+            switch (item.date.getMonth()){
+                case 0: return 'Январь';
+                case 1: return 'Февраль';
+                case 2: return 'Март';
+                case 3: return 'Апрель';
+                case 4: return 'Май';
+                case 5: return 'Июнь';
+                case 6: return 'Июль';
+                case 7: return 'Август';
+                case 8: return 'Сентябрь';
+                case 9: return 'Октябрь';
+                case 10: return 'Ноябрь';
+                case 11: return 'Декабрь';
+            }
+        };
+
+        /*СПИСОК И ВВОД*/
+
+        //Начальный список
         $scope.list = [
             // createListItem('Помидоры','100', 'outcome', 1),
             createListItem('Зарплата', '200', 'income', 1)
         ];
 
+        //Объект ввода
         $scope.input = {
             name: '',
             sum: '',
             category: 'income',
         };
 
-        $scope.emptyName = false;
-        $scope.emptySum = false;
-        $scope.wrongSum = false;
-        $scope.currMonth = 12;
-        $scope.monthName = function (item) {
-          switch (item.date.getMonth()){
-              case 0: return 'Январь';
-              case 1: return 'Февраль';
-              case 2: return 'Март';
-              case 3: return 'Апрель';
-              case 4: return 'Май';
-              case 5: return 'Июнь';
-              case 6: return 'Июль';
-              case 7: return 'Август';
-              case 8: return 'Сентябрь';
-              case 9: return 'Октябрь';
-              case 10: return 'Ноябрь';
-              case 11: return 'Декабрь';
-          }
-        };
+        //Создание нового элемента списка
+        function createListItem(name, sum, category, date) {
+            date = new Date();
+            //КОСТЫЛЬ ДЛЯ ПРОВЕРКИ ГРУППИРОВКИ ПО МЕСЯЦАМ
+            if(arguments.length === 4)
+                date.setMonth(6);
+            return {
+                name: name,
+                sum: sum,
+                category: category,
+                date: date,
+                newMonth: false
+            }
+        }
 
-        $scope.balance = 0;
-
-        $scope.menu = [{name: 'все'}, {name: 'доходы', category: 'income'},
-            {name: 'расходы', category: 'outcome'}];
-
-        $scope.selectedMenuItem = $scope.menu[0];
-
+        //Удаление элемента списка с изменением баланса
         $scope.deleteListItem = function (index) {
             if($scope.list[index].category === 'income'){
                 $scope.balance -= Number($scope.list[index].sum);
@@ -51,18 +69,7 @@ angular.module('app', [])
             $scope.list.splice(index, 1);
         };
 
-        $scope.selectMenuItem = function (item) {
-            $scope.selectedMenuItem = item;
-        };
-
-        $scope.getFilteredListItem = function () {
-            if (!$scope.selectedMenuItem.category){
-                return $scope.list;
-            }
-
-            return $scope.list.filter(i => i.category === $scope.selectedMenuItem.category);
-        };
-
+        //Добавление нового элемента в список
         $scope.addListItem = function ($event) {
 
             if($event.keyCode !== 13 && $event.type !== 'click') {
@@ -109,19 +116,30 @@ angular.module('app', [])
             $scope.input.category = 'income';
         };
 
-        function createListItem(name, sum, category, date) {
-            date = new Date();
-            if(arguments.length === 4)
-                date.setMonth(6);
-            return {
-                name: name,
-                sum: sum,
-                category: category,
-                date: date,
-                newMonth: false
-            }
-        }
+        /*МЕНЮ*/
 
+        //Пункты меню
+        $scope.menu = [{name: 'все'}, {name: 'доходы', category: 'income'},
+            {name: 'расходы', category: 'outcome'}, {name: 'статистика', category: 'statistic'}];
+
+        //Глобальные переменные
+        $scope.selectedMenuItem = $scope.menu[0];
+
+        //Выбираем новый пункт меню
+        $scope.selectMenuItem = function (item) {
+            $scope.selectedMenuItem = item;
+        };
+
+        //Фильтрация общего списка для вывода в меню
+        $scope.getFilteredListItem = function () {
+            if (!$scope.selectedMenuItem.category){
+                return $scope.list;
+            }
+
+            return $scope.list.filter(i => i.category === $scope.selectedMenuItem.category);
+        };
+
+        //Пустая ли строка
         function isEmptyString(str) {
             return /^\s*$/.test(str);
         }
