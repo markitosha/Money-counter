2bd135becb46f11128a860e17db2fcf90e82dc70
markitosha <nataly_106@mail.ru>
2016-08-22 13:53:00 +0300

    Groupping by months added.

--- app.js
+++ app.js
@@ -4,19 +4,36 @@
 angular.module('app', [])
     .controller('main', function($scope){
         $scope.list = [
-            // createListItem('Помидоры','100', 'outcome'),
-            // createListItem('Зарплата', '200', 'income')
+            // createListItem('Помидоры','100', 'outcome', 1),
+            createListItem('Зарплата', '200', 'income', 1)
         ];
 
         $scope.input = {
             name: '',
             sum: '',
-            category: 'income'
+            category: 'income',
         };
 
         $scope.emptyName = false;
         $scope.emptySum = false;
         $scope.wrongSum = false;
+        $scope.currMonth = 12;
+        $scope.monthName = function (item) {
+          switch (item.date.getMonth()){
+              case 0: return 'Январь';
+              case 1: return 'Февраль';
+              case 2: return 'Март';
+              case 3: return 'Апрель';
+              case 4: return 'Май';
+              case 5: return 'Июнь';
+              case 6: return 'Июль';
+              case 7: return 'Август';
+              case 8: return 'Сентябрь';
+              case 9: return 'Октябрь';
+              case 10: return 'Ноябрь';
+              case 11: return 'Декабрь';
+          }
+        };
 
         $scope.balance = 0;
 
@@ -75,6 +92,12 @@ angular.module('app', [])
 
             $scope.list.splice(0, 0, createListItem(name, sum, category));
 
+            var nm = $scope.currMonth !== $scope.list[0].date.getMonth();
+            $scope.currMonth = $scope.list[0].date.getMonth();
+            if($scope.list.length > 1){
+                $scope.list[1].newMonth = nm;
+            }
+
             if(category === 'income'){
                 $scope.balance += Number(sum);
             }else{
@@ -86,14 +109,16 @@ angular.module('app', [])
             $scope.input.category = 'income';
         };
 
-        function createListItem(name, sum, category) {
+        function createListItem(name, sum, category, date) {
             date = new Date();
-            date.toTimeString();
+            if(arguments.length === 4)
+                date.setMonth(6);
             return {
                 name: name,
                 sum: sum,
                 category: category,
-                date: new Date(),
+                date: date,
+                newMonth: false
             }
         }
 
